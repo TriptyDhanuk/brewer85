b137290047c949a16a03814aa3cb81289edae835
sonaiClayMind <sonaiClayMind@gmail.com>
2024-02-21 17:57:55 +0530

    added OTP Validation page

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -49,7 +49,7 @@ const GetStartedButton = styled.button`
   cursor: pointer;
   font-family: 'Raleway', sans-serif;
   transition: background-color 0.3s;
-  z-index: 1;
+  z-index: 2; /* Ensure button appears above images */
 
   &:hover {
     background-color: #444; /* Darken button color on hover */
@@ -59,6 +59,7 @@ const GetStartedButton = styled.button`
     width: 8rem; 
     height: 2.5rem; 
     font-size: 1rem;
+    z-index: 2; /* Ensure button appears above images */
   }
 `;
 
@@ -78,28 +79,45 @@ const BackgroundImage = styled.div`
 
   @media screen and (max-width: 768px) {
     top: 0rem;
-    right: 5rem;
+    right: 3rem;
     width: 30rem;
     height: 40rem;
-    transform: rotate(30deg);
+    transform: rotate(40deg);
   }
 
   @media screen and (max-width: 480px) {
-    top: -3rem;
+    top: 5rem;
     right: 2rem;
     width: 15rem;
     height: 20rem;
-    transform: rotate(20deg);
+    transform: rotate(40deg);
   }
 `;
+
 const Item2Image = styled.img`
   position: absolute;
   bottom: -1rem; /* Adjust bottom spacing as needed */
   left: 10rem; /* Adjust left spacing as needed */
   width: 25rem;
   height: 25rem; 
-  z-index: 1; /* Ensure the image appears above the background color */
+
+
+  @media screen and (max-width: 768px) {
+    width: 20rem;
+    height: 20rem;
+    bottom: 2rem;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: -1; /* Adjust z-index to move the image below the button */
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 15rem;
+    height: 15rem;
+    z-index: -1; /* Adjust z-index to move the image below the button */
+  }
 `;
+
 
 
 const Start = () => {
@@ -120,5 +138,3 @@ const Start = () => {
 };
 
 export default Start;
-
-

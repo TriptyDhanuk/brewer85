78a0dc66f3d8b1aaa7b028dd462ae452bb2db349
sonaiClayMind <sonaiClayMind@gmail.com>
2024-02-27 16:48:01 +0530

    added nav bar wishlist & cart together

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,3 +1,22 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import { selectCartTotalItems } from "../features/cart/cartSlice";
+
+// const Wishlist = () => {
+//   const totalItems = useSelector(selectCartTotalItems);
+
+//   return (
+//     <Link to="/wishlist" className="cart-icon-container">
+//       <box-icon type="solid" name="heart">
+//         {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
+//       </box-icon>
+//     </Link>
+//   );
+// };
+
+// export default Wishlist;
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,12 +26,13 @@ const Wishlist = () => {
   const totalItems = useSelector(selectCartTotalItems);
 
   return (
-    <Link to="/wishlist" className="cart-icon-container">
-      <box-icon type="solid" name="heart">
+    <Link to="/wishlist" className="wishlist-icon-container">
+      <i className="fas fa-heart" style={{ marginRight: "1.8rem", position: "relative" }}>
         {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
-      </box-icon>
+      </i>
     </Link>
   );
 };
 
 export default Wishlist;
+

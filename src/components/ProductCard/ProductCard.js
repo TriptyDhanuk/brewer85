df1e3db94173c92e895ff8e7aeaad9c21ff11fde
sonaiClayMind <sonaiClayMind@gmail.com>
2024-04-01 14:02:21 +0530

    added conditional rendering the add cart button in menu page

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -13,11 +13,14 @@ import { selectCartItems } from "../../features/cart/selectors";
 import WishlistNotification from "../Notificaiton/WishlistNotification.js";
 import Notification from "../Notificaiton/Notification.js";
 import { selectSavedForLaterItems } from "../../features/cart/wishlistSlice";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ id, image, name, price, discount }) => {
+  const location=useLocation();
   const saveItems = useSelector(selectSavedForLaterItems);
   const cartItems = useSelector(selectCartItems);
   const [isRemoved, setIsRemoved] = useState(false);
+  const isMenuPage = location.pathname === "/menu";
 
   const [quantity, setQuantity] = useState(1);
   const [notification, setNotification] = useState(null);
@@ -177,87 +180,87 @@ const ProductCard = ({ id, image, name, price, discount }) => {
 
             
 
-            <div className="flex items-center">
-              <div className="quantity-button w-1/2">
-                <div
-                  className="py-2 px-2 inline-block bg-white border border-gray-200 rounded-lg dark:bg-slate-900 dark:border-gray-700"
-                  data-hs-input-number
-                >
-                  <div className="flex items-center gap-x-1.5">
-                    <button
-                      className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                      onClick={handleMinusClick}
-                    >
-                      <svg
-                        className="flex-shrink-0 size-3.5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
+              {isMenuPage && (<div className="flex items-center">
+                <div className="quantity-button w-1/2">
+                  <div
+                    className="py-2 px-2 inline-block bg-white border border-gray-200 rounded-lg dark:bg-slate-900 dark:border-gray-700"
+                    data-hs-input-number
+                  >
+                    <div className="flex items-center gap-x-1.5">
+                      <button
+                        className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        onClick={handleMinusClick}
                       >
-                        <path d="M5 12h14" />
-                      </svg>
-                    </button>
-                    <input
-                      type="text"
-                      value={quantity}
-                      readOnly
-                      className="p-0 w-6 bg-transparent border-0 text-gray-800 text-center focus:ring-0 dark:text-white"
-                    />
-                    <button
-                      className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                      onClick={handlePlusClick}
-                    >
-                      <svg
-                        className="flex-shrink-0 size-3.5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
+                        <svg
+                          className="flex-shrink-0 size-3.5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14" />
+                        </svg>
+                      </button>
+                      <input
+                        type="text"
+                        value={quantity}
+                        readOnly
+                        className="p-0 w-6 bg-transparent border-0 text-gray-800 text-center focus:ring-0 dark:text-white"
+                      />
+                      <button
+                        className="size-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        onClick={handlePlusClick}
                       >
-                        <path d="M5 12h14" />
-                        <path d="M12 5v14" />
-                      </svg>
-                    </button>
+                        <svg
+                          className="flex-shrink-0 size-3.5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M5 12h14" />
+                          <path d="M12 5v14" />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="w-1/2">
-                {/* <button
-                  className="py-2 px-5 w-full font-semibold text-lime-600 border border-solid border-lime-600 rounded-md hover:text-white hover:bg-lime-600 duration-150"
-                  onClick={handleAddToCart}
-                >
-                  ADD
-                </button> */}
-                {existingCartItem ? (
-                    <Link to="/checkout">
+                <div className="w-1/2">
+                  {/* <button
+                    className="py-2 px-5 w-full font-semibold text-lime-600 border border-solid border-lime-600 rounded-md hover:text-white hover:bg-lime-600 duration-150"
+                    onClick={handleAddToCart}
+                  >
+                    ADD
+                  </button> */}
+                  {existingCartItem ? (
+                      <Link to="/checkout">
+                        <button
+                          className="py-2 px-5 w-full font-semibold uppercase text-lime-600 border border-solid border-lime-600 rounded-md hover:text-white hover:bg-lime-600 duration-150 ml-2"
+                        >
+                            CART
+                        </button>
+                      </Link>
+                    ) : (
                       <button
                         className="py-2 px-5 w-full font-semibold uppercase text-lime-600 border border-solid border-lime-600 rounded-md hover:text-white hover:bg-lime-600 duration-150 ml-2"
+                        onClick={handleAddToCart}
                       >
-                          CART
+                        ADD
                       </button>
-                    </Link>
-                  ) : (
-                    <button
-                      className="py-2 px-5 w-full font-semibold uppercase text-lime-600 border border-solid border-lime-600 rounded-md hover:text-white hover:bg-lime-600 duration-150 ml-2"
-                      onClick={handleAddToCart}
-                    >
-                      ADD
-                    </button>
-                  )}
-              </div>
-            </div>
+                    )}
+                </div>
+              </div>)}
           </div>
         </div>
       </div>

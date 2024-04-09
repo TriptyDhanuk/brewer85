bb9a1d5e95242661d765adeddc89be38b7c29c04
subhankar86 <sprasadcm2016@gmail.com>
2024-04-09 11:44:10 +0530

    fixed product design

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -131,13 +131,13 @@ const ProductCard = ({ id, image, name, price, discount }) => {
   };
   return (
     <div className="relative mx-[0.625rem]">
-      <div className="product-card border border-solid border-slate-200 rounded-lg relative h-[280px] overflow-hidden">
+      <div className="product-card border border-solid border-slate-200 rounded-lg relative h-[280px]">
         <div className="best-seller-ribbon absolute top-2 -left-[0.625rem] text-white py-2 pl-5 pr-2 font-bold bg-lime-600 after:content-[''] after:absolute after:top-0 after:right-[-19px] after:border-t-[18px] after:border-l-[10px] after:border-b-[18px] after:border-r-[10px] after:border-t-lime-600 after:border-r-transparent after:border-b-lime-600 after:!border-l-lime-600 before:content-[''] before:absolute before:top-[100%] before:left-0 before:border-t-[10px] before:border-l-[10px] before:border-b-[10px] before:border-t-lime-900 before:border-l-transparent  before:border-b-transparent text-sm">Best Seller</div>
         {/* <p className="absolute top-0">{id}</p> */}
         <img
           src={image}
           alt={name}
-          className="product-image w-full h-full object-cover"
+          className="product-image w-full h-full object-cover rounded-lg"
           onClick={handleImgClick}
         />
         <div onClick={handleImgClick} className="rounded-lg absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-t from-black to-transparent opacity-75"></div>
